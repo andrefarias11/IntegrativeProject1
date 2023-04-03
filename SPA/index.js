94ebb9db7e9939a3baca4a7d8739a4f7ad1a62f5
AndreFarias <andrefarias389@gmail.com>
2023-04-03 19:28:36 -0300

    Start Web

---
+++ SPA/index.js
@@ -0,0 +1,8 @@
+function trocarCor() {
+  var body = document.querySelector("body");
+  if (body.style.backgroundColor === "gray") {
+    body.style.backgroundColor = "white";
+  } else {
+    body.style.backgroundColor = "gray";
+  }
+}
